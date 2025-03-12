51b3f91c928a3cc3f41d2815215046dfebbca24e
Ezequiel Gonzalez <ezequielgonzalez@MacBookPro.lan>
2025-03-12 14:10:59 -0400

    Subiendo código del proyecto

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const WebSocket = require("ws");
+const http = require("http");
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws) => {
+    console.log("Cliente conectado");
+
+    ws.on("message", (message) => {
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.on("close", () => {
+        console.log("Cliente desconectado");
+    });
+});
+
+app.use(express.static("public"));
+
+server.listen(3000, () => {
+    console.log("Servidor corriendo en http://localhost:3000");
+});
+
